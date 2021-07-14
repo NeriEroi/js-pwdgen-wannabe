7f82b7130f2236987765d9cc8c50804e3a0b8441
Michele <micheleierniero@gmail.com>
2021-07-14 14:14:15 +0200

    bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,40 @@
+// what's your name?
+
+var yourname
+
+yourname = prompt("Potresti dirmi qual è il tuo nome?")
+
+console.log(yourname)
+
+
+// what's your surname?
+
+var surname
+
+surname = prompt("Potresti dirmi qual è il tuo cognome?")
+
+console.log(surname)
+
+
+// what's your favourite color?
+
+var yourcolor
+
+yourcolor = prompt("Potresti dirmi qual è il tuo colore preferito?")
+
+console.log(yourcolor)
+
+
+// What's your age?
+
+var age
+
+age = prompt("Potresti dirmi qual è la tua età?")
+
+console.log(age)
+
+
+// all four info togheter
+
+document.getElementById("allfourinfo").innerHTML="MicheleIernieroarancione31";
+
